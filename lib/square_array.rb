c074f3dc4237b0d03431085b3389f77ce3ae1416
Stephanie Zou <stephanie.zou@stern.nyu.edu>
2020-02-15 17:20:46 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -2,7 +2,7 @@ def square_array(array)
   output = []
   counter = 0 
   while counter < array.length do    
-    output[counter] = array[counter] ** 2 
+    output.push(array[counter] **2) 
     puts output[counter]
     counter += 1
   end
